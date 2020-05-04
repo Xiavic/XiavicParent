81eb92ebb479abc2be4559e600e1d5535fb2097d
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-05-04 09:58:17 +0200

    Squashed 'core/' content from commit bc9a2d1
    git-subtree-dir: core
    git-subtree-split: bc9a2d115a42715c8dd398036d657f5728573921

---
+++ src/main/java/com/github/prypurity/xiaviccore/Commands/StaffCmds/noncheat/SudoCommand.java
@@ -0,0 +1,131 @@
+package com.github.prypurity.xiaviccore.Commands.StaffCmds.noncheat;
+
+import com.github.prypurity.xiaviccore.Utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.github.prypurity.xiaviccore.Main.messages;
+import static com.github.prypurity.xiaviccore.Main.permissions;
+
+/**
+ * Represents a command for sudo.
+ */
+public class SudoCommand implements TabExecutor {
+
+    @Override //Target: /sudo <player> <command> [args]
+    public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+        @NotNull final String label, @NotNull final String[] args) {
+
+        if (sender.hasPermission(permissions.getString("Sudo")) || !sender.isOp()) {
+            if (args.length > 1) {
+                final Player player = Bukkit.getPlayer(args[0]);
+                if (player != null) {
+                    if (!args[1].equalsIgnoreCase("s:")) { //Send chat message as the player.
+                        final Command target = Bukkit.getPluginCommand(args[1]);
+                        if (target != null) {
+                            final String[] arr = Arrays.copyOfRange(args, 2, args.length);
+                            target.execute(player, args[1], arr);
+                        } else {
+                            Utils.chat(player, messages.getString("NoSuchCommand"));
+                        }
+                    } else {
+                        StringBuilder builder = new StringBuilder();
+                        for (int index = 2; index < args.length; ) {
+                            builder.append(args[index++]);
+                        }
+                        player.chat(builder.toString()); //TODO Needs testing
+                    }
+                    ;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            Utils.chat(sender, messages.getString("NoPerms"));
+            return true;
+        }
+    }
+
+    /**
+     * Tab completer for sudo.
+     * This tab-completer will attempt to tab complete:
+     * - The player to execute as
+     * - The command to execute as (in the context of the player)
+     * - The args of the command {@link Command#tabComplete(CommandSender, String, String[])}
+     * {@inheritDoc}
+     */
+    @Override public @Nullable List<String> onTabComplete(@NotNull final CommandSender sender,
+        @NotNull final Command command, @NotNull final String unusedAlias,
+        @NotNull final String[] args) {
+        //System.out.println(sender.isOp());
+        if (!sender.hasPermission(permissions.getString("Sudo")) && !sender.isOp()) {
+            return Collections.emptyList();
+        }
+        final Stream<? extends Command> commands =
+            Utils.generateCommandMap().getCommands().stream().filter((cmd) -> {
+                if (!sender.isOp()) {
+                    if (cmd.getPermission() != null) {
+                        return sender.hasPermission(cmd.getPermission());
+                    }
+                }
+                return true;
+            }); //Get all commands.
+
+        final Iterator<? extends Command> iterator = commands.iterator();
+        Collection<String> combined = new HashSet<>();
+        while (iterator.hasNext()) {
+            //Combine command label and aliases.
+            final Command cmd = iterator.next();
+            combined.add(cmd.getLabel());
+            combined.addAll(cmd.getAliases());
+        }
+        Stream<String> ret = combined.stream();
+        switch (args.length) {
+            case 0:
+                ret = Bukkit.getOnlinePlayers().stream().map(Player::getName);
+                break;
+            case 1: //Tab complete player names.
+                ret = Bukkit.getOnlinePlayers().stream().map(Player::getName)
+                    .filter(name -> name.startsWith(args[0]) || name.equalsIgnoreCase(args[0]));
+                break;
+            case 2:  //If no args after player decleration, tab complete all commands which the player has perms for.
+                //Start filtering commands and aliases.
+                final String targetCommand = args[1];
+                if (!targetCommand.isEmpty()) {
+                    if (!targetCommand.equalsIgnoreCase("s:")) {
+                        ret = ret.filter(str -> str.startsWith(targetCommand) || str
+                            .equalsIgnoreCase(targetCommand));
+                    } else {
+                        ret = Stream.empty();
+                    }
+                }
+                break;
+            default: //If arg length > 2 then tab based off the known command.
+                final Player targetPlayer = Bukkit.getPlayer(args[0]);
+                final PluginCommand pluginCommand =
+                    Bukkit.getPluginCommand(args[1]); //args[1] is the target command.
+                if (pluginCommand != null) {
+                    ret = targetPlayer == null ?
+                        Stream.empty() :
+                        //If player is null --> empty stream.
+                        pluginCommand.tabComplete(targetPlayer, unusedAlias,
+                            Arrays.copyOfRange(args, 2, args.length)).stream();
+                } else {
+                    ret = Stream.empty();
+                }
+        }
+        //Returns the alphabetically sorted results.
+        return ret.sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/github/prypurity/xiaviccore/Utils/Utils.java
@@ -0,0 +1,99 @@
+package com.github.prypurity.xiaviccore.Utils;
+
+import com.github.prypurity.xiaviccore.Main;
+import com.github.prypurity.xiaviccore.Utils.Listeners.TeleportHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+
+
+public class Utils {
+
+    //EZ Chat Colors
+
+    public static String chat(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    // Sends messages to a player directly and makes the 'chat' name make more sense
+    public static void chat(Player player, String string) {
+        player.sendMessage(chat(string));
+    }
+
+    // An overload so you can do the same thing when you need to send a message to console
+    // from inside a command class
+    public static void chat(CommandSender sender, String string) {
+        sender.sendMessage(chat(string));
+    }
+
+    /**
+     * Attempt to replace the currently held item to a new one. The old item will be moved to
+     * the next empty slot.
+     *
+     * @param player    The player's {@link Player} instance.
+     * @param itemStack The {@link ItemStack} to replace - if null, air will be set instead.
+     * @return Returns whether or not the swap was successful.
+     */
+    public static boolean placeInCursorSlot(@NotNull final Player player,
+        @Nullable final ItemStack itemStack) {
+        final PlayerInventory inventory = player.getInventory();
+        final int firstEmpty = inventory.firstEmpty();
+        if (firstEmpty == -1) {
+            return false;
+        }
+        final int held = inventory.getHeldItemSlot();
+        inventory.setItem(firstEmpty, inventory.getItem(held));
+        inventory.setItem(held, itemStack == null ? null : itemStack.clone());
+        return true;
+    }
+
+    public static long toTicks(final long duration, final TimeUnit timeUnit) {
+        return TimeUnit.MILLISECONDS.convert(duration, timeUnit) * 50; //Each tick is 50ms
+    }
+
+    public static long fromTicks(final long ticks, final TimeUnit timeUnit) {
+        return timeUnit.convert(ticks / 50, timeUnit);
+    }
+
+    @Deprecated public static void teleport(Player player, Location location) {
+        TeleportHandler.processPlayerTeleport(player);
+        player.teleport(location);
+    }
+
+    /**
+     * Generates the commandMap for the server.
+     * @return Returns the {@link SimpleCommandMap} object for the server.
+     */
+    public static SimpleCommandMap generateCommandMap() {
+        Server server = Bukkit.getServer();
+        try {
+            Class<?> clazz = server.getClass();
+            Field field = clazz.getDeclaredField("commandMap");
+            field.setAccessible(true);
+            return (SimpleCommandMap) field.get(server);
+        } catch (final Exception ex) {
+            throw new IllegalStateException("Unable to get commandMap!", ex);
+        }
+    }
+
+    // This teleport method lets you send a message to the player here instead of
+    // having to do it where ever you called this method
+    @Deprecated public static void teleport(Player player, Location location, String messagePath) {
+        TeleportHandler.processPlayerTeleport(player);
+        player.teleport(location);
+        chat(player, Main.messages.getString(messagePath));
+    }
+
+
+}
