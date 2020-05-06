fdb324e721112a6b79e4271b63977d33a663f678
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-05-06 15:31:29 +0200

    Fully implement /signedit

--- bukkit-1.15/src/main/java/com/github/prypurity/xiaviccore/v1_15_R1/NMSImpl.java
+++ bukkit-1.15/src/main/java/com/github/prypurity/xiaviccore/v1_15_R1/NMSImpl.java
@@ -1,7 +1,7 @@
 package com.github.prypurity.xiaviccore.v1_15_R1;
 
 
-import com.github.prypurity.xiaviccore.Utils.ISignEditor;
+import com.github.prypurity.xiaviccore.Utils.signedit.ISignEditor;
 import com.github.prypurity.xiaviccore.Utils.NMSHandler.NMS;
 import com.github.prypurity.xiaviccore.Utils.inventory.InventorySerializer;
 
@@ -9,8 +9,10 @@ public class NMSImpl implements NMS {
 
     public NMSImpl() {}
 
+    private final ISignEditor signEditor = new SimpleSignEditor();
+
     @Override public ISignEditor getSignEditor() {
-        throw new UnsupportedOperationException();
+        return signEditor;
     }
 
     @Override public InventorySerializer getInventorySerializer() {
--- bukkit-1.15/src/main/java/com/github/prypurity/xiaviccore/v1_15_R1/SimpleSignEditor.java
+++ bukkit-1.15/src/main/java/com/github/prypurity/xiaviccore/v1_15_R1/SimpleSignEditor.java
@@ -1,29 +1,31 @@
 package com.github.prypurity.xiaviccore.v1_15_R1;
 
-import com.github.prypurity.xiaviccore.Utils.ISignEditor;
+import com.github.prypurity.xiaviccore.Utils.signedit.AbstractSignEditor;
 import net.minecraft.server.v1_15_R1.*;
 import org.bukkit.block.Sign;
-import org.bukkit.craftbukkit.v1_15_R1.block.CraftSign;
+import org.bukkit.craftbukkit.v1_15_R1.block.CraftBlockEntityState;
 import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
 
-public class SimpleSignEditor implements ISignEditor {
+public class SimpleSignEditor extends AbstractSignEditor {
 
-    @Override public Sign openUI(final Player player) {
-        final CraftSign sign = new CraftSign(player.getLocation().getBlock());
+    @Override public void openUI(final @NotNull Player player, final @NotNull Sign sign) {
         final EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
         final PlayerConnection connection = entityPlayer.playerConnection;
         final TileEntitySign tileSign;
         try {
-            final Field tileField = sign.getClass().getDeclaredField("sign");
+            final Field tileField = CraftBlockEntityState.class.getDeclaredField("tileEntity");
+            //For 1.15 mappings - this is now under CraftBlockTileEntity<TileEntitySign>
             tileField.setAccessible(true);
             // Gets the tileEntity sign for later use
             tileSign = (TileEntitySign) tileField.get(sign);
         } catch (final ReflectiveOperationException ex) {
             throw new IllegalStateException("Unable to open sign!", ex);
         }
+        tileSign.isEditable = true;
         // Makes the TileEntity sign editable so when we close the ui it
         // doesn't undo the changes
         tileSign.a(entityPlayer);
@@ -37,6 +39,5 @@ public class SimpleSignEditor implements ISignEditor {
         connection.sendPacket(packet);
         // This sends the packet to the playerconnection so the player
         // "opens" the sign
-        return sign;
     }
 }
