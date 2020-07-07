eb9d41f3723bb227602babe58f343f2e36e40b98
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-07-08 00:34:44 +0200

    Migrate most of the staff commands over to ACF command handlers
    This commit has minor fixes to migrate legacy message sending over to
    the new message format using MiniMessage. All commands which are simple
    have been migrated to using ACF format bar ones which are incomplete.
    Some renaming has been done for warp classes.

--- XiavicLib/src/main/java/com/github/xiavic/lib/teleport/ITeleportHandler.java
+++ XiavicLib/src/main/java/com/github/xiavic/lib/teleport/ITeleportHandler.java
@@ -3,17 +3,19 @@ package com.github.xiavic.lib.teleport;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface ITeleportHandler {
     void processPlayerTeleport(Player player);
 
     void processPlayerToggle(Player player);
 
-    void teleport(Player player, Location location);
+    CompletableFuture<Boolean> teleport(Player player, Location location);
 
     // change - if true: teleport player2 to player1 else teleport player1 to player2
-    boolean teleport(Player p1, Player p2, boolean change);
+    CompletableFuture<Boolean> teleport(Player p1, Player p2, boolean change);
 
-    boolean remoteTp(Player player, Location location);
+    CompletableFuture<Boolean> remoteTp(Player player, Location location);
 
     // 0 - teleport successful
     // 1 - player1 disabled
