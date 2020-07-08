679d21b9fc3547956e9dc3be30e288a616203422
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-07-08 21:59:49 +0200

    Fix runtime errors regarding ACF and NMS

--- XiavicLib/src/main/java/com/github/xiavic/lib/NMSHandler/NMSVersion.java
+++ XiavicLib/src/main/java/com/github/xiavic/lib/NMSHandler/NMSVersion.java
@@ -4,14 +4,14 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 public enum NMSVersion {
-    V1_8_R1, V1_8_R2, V1_8_R3, V1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, V1_12_R1, v1_13_R1, V1_13_R2, V1_14_R1, V1_15_R1, V1_16_R1;
+    v1_8_R1, v1_8_R2, v1_8_R3, v1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, v1_12_R1, v1_13_R1, v1_13_R2, v1_14_R1, v1_15_R1, v1_16_R1;
 
     NMSVersion() {
     }
 
     public static NMSVersion getCurrent() throws IllegalArgumentException {
-        return NMSVersion.valueOf("V" + Bukkit.getServer().getClass().getPackage().getName()
-            .replace("org.bukkit.craftbukkit", ""));
+        return NMSVersion.valueOf(Bukkit.getServer().getClass().getPackage().getName()
+            .replace("org.bukkit.craftbukkit.", ""));
     }
 
     @NotNull public String getNumericalName() {
@@ -41,10 +41,10 @@ public enum NMSVersion {
     }
 
     public boolean isNewerThan(@NotNull final NMSVersion other) {
-        return other.ordinal() > this.ordinal();
+        return this.ordinal() > other.ordinal();
     }
 
     public boolean isOlderThan(@NotNull final NMSVersion other) {
-        return other.ordinal() < this.ordinal();
+        return this.ordinal() < other.ordinal();
     }
 }
